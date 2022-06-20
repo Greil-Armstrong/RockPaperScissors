788665c4b55d0b00e2addf52174e1e0e912bafb8
Thibaut <thcantonnet@gmail.com>
2022-06-20 16:44:07 +0200

    Add function returning rock/paper/scissors by the computer to a variable

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+// Faire attribuer à une autre variable de manière aléatoire "Rock / Paper / Scissor" par l'ordinateur.
+function computerPlay() {
+    const computerSelection = () => {
+        const randomNumber = (Math.floor(Math.random() * 3))
+        if (randomNumber === 0){
+          return 'rock';
+        }
+        else if (randomNumber === 1){
+          return 'paper';
+        }
+        else if (randomNumber === 2){
+        return 'scissor';
+        }
+          }
+      console.log(computerSelection());
+}
+
+// Demander utilisateur input "Rock / Paper / Scissor" et l'attribuer à une variable.
+const question = "Choose between Rock Paper Scissors."
+const playerSelection = prompt(question).toLowerCase();
+const computerSelection = computerPlay()
+
+// Comparer les valeurs attribuer aux deux variables et retourner le résultat.
